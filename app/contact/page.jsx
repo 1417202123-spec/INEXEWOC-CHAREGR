740eab68614277fff787d9814c79903dfa8ceda2
1417202123-spec <1417202123@qq.com>
2026-04-17 18:05:04 +0800

    Delete app/contact directory

--- app/contact/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <div style={{ padding: '40px 8%' }}>
-      <h1>Contact Us</h1>
-      <p>Email: your@mail.com</p>
-      <p>WhatsApp: +86 XXXXXXXX</p>
-    </div>
-  );
-}
